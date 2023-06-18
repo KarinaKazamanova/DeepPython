16a81b48d802f905c7c7d1966ab00beed009b5d2
Stanley <k5524392@gmail.com>
2023-06-18 09:24:31 +0300

    task1

---
+++ Семинар5/task1.py
@@ -0,0 +1,11 @@
+# ✔Напишите функцию, которая принимает на вход строку — абсолютный путь до файла. 
+# Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+
+import os
+
+
+
+def get_file_info(path):
+    return path, path.split("\\")[-1], path.split("\\")[-1].split(".")[-1]
+
+print(get_file_info(os.path.abspath('task1.txt')))
