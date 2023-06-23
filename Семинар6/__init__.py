f65e0f8b0930cac0345b7b452a72a4d91b599429
Stanley <k5524392@gmail.com>
2023-06-23 23:18:44 +0300

    chess_riddle

--- Семинар6/__init__.py
+++ Семинар6/__init__.py
@@ -1,6 +1,7 @@
 from .guess import guess_f
 from .modul_ import zagadka, fun_2, puzzle
 from .year_ import my_date
+from .chess_ import eight_queens_solution
 
 
-__all__ = ['guess_f', 'zagadka', 'fun_2', 'puzzle', 'my_date']
+__all__ = ['guess_f', 'zagadka', 'fun_2', 'puzzle', 'my_date','eight_queens_solution']
